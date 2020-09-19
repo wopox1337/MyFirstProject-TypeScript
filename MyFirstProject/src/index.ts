5a30795cb319e28bd78c49e2d0cfc2cb7cfe38ce
SergeyShorokhov <wopox1337@ya.ru>
2020-09-19 04:01:46 +0300

    Init

---
+++ MyFirstProject/src/index.ts
@@ -0,0 +1,39 @@
+import "reflect-metadata";
+import {Connection, createConnection} from "typeorm";
+
+import {User} from "./entity/User";
+
+interface IUser {
+    firstName: string,
+    lastName: string,
+    age: number,
+}
+
+createConnection().then(async connection => {
+    const usersList: IUser[] = [
+        { firstName: "Vasya", lastName: "Pupkin", age: 26 },
+        { firstName: "Petya", lastName: "Lupkin", age: 22 },
+    ];
+
+    const promises = usersList.map((item) => AddUser(connection, item));
+    await Promise.all(promises);
+
+    console.log("Loading users from the database...");
+    
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
+
+    await connection.manager.clear(User);
+    console.log("Table cleared.");
+}).catch(error => console.log(error));
+
+async function AddUser(connection: Connection, userData: IUser) {
+    console.log("Inserting a new user into the database...");
+
+    const user = new User();
+    user.firstName = userData.firstName;
+    user.lastName = userData.lastName;
+    user.age = userData.age;
+
+    return connection.manager.save(user);
+}
